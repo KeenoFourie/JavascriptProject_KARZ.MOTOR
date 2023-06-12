01cc5b316d2543944d707d5d72314092da6d8f1d
KeenoFourie <keenofourie614@gmail.com>
2023-06-12 16:35:30 +0200

    admin not done

--- script/admin.js
+++ script/admin.js
@@ -1,163 +1,154 @@
 // DOM
-let result = document.querySelector("#blogs");
-let addBtn = document.querySelector(".newblog");
-let tempID = 7;
-let tempTitle = document.querySelector(".input1");
-let tempImg = document.querySelector(".input2");
-let tempDep = document.querySelector(".input3");
-let tempCon = document.querySelector(".input4");
+let display = document.querySelector("#product_list");
+let addBtn = document.querySelector("#new_product");
+let product_id = 7;
+let product_model = document.querySelector("#input_1");
+let product_image = document.querySelector("#input_2");
+let product_description = document.querySelector("#input_3");
+let product_price = document.querySelector("#input_4");
 
 
 
 
-// Admin Object
-let  featuredPosts = [
+// Admin Products(objects)
+let  featured_products = [
     {
-    id: " 1 ",
-    title: " 2023 Video Game Release Date Calendar ",
-    imageUrl: " https://slack-imgs.com/?c=1&o1=ro&url=https%3A%2F%2Fstatic0.gamerantimages.com%2Fwordpress%2Fwp-content%2Fuploads%2F2023%2F01%2F2023-release-gaming-calendar-game-rant.jpg%3Fq%3D50%26fit%3Dcontain%26w%3D1140%26h%3D%26dpr%3D1.5 ",
-    description: " Check out this guide to find out the release dates for every major video game coming out in 2023. ",
-    content: " BY GREYSUN MORALES ",
-    date: new Date( )
+    id: "1",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     },
     {
-    id: " 2 ",
-    title: " Will Smith confronts Chris Rock over Jada Pinkett Smith “joke” at Oscars 2022 ",
-    imageUrl: " https://www.nme.com/wp-content/uploads/2022/03/will-smith-oscars-2022-696x442.jpg ",
-    description: " The actor later apologised for his actions while collecting the award for Best Actor, saying: `Love will make you do crazy things`",
-    content: " By Rhian Daly ",
-    date: new Date( )
+    id: "2",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     },
     {
-    id: " 3 ",
-    title: " Declan Rice delivers pulsating final act to push West Ham over the line ",
-    imageUrl: " https://i.guim.co.uk/img/media/045af41da88d37ecceb40e56369fd67b368fad0b/0_164_3500_2100/master/3500.jpg?width=620&quality=45&dpr=2&s=none ",
-    description: " Fiorentina impressed but David Moyes's side dug deep thanks to their relentless captain who is expected to leave this summer ",
-    content: " By Ed Aarons ",
-    date: new Date( )
+    id: "3",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     },
     {
-    id: " 4 ",
-    title: " The Dark Assailant - A Short Story ",
-    imageUrl: " https://thedarkestblog.com/wp-content/uploads/2022/12/dark-assailant-595x448.jpg ",
-    description: " A faint ray of light was still burning in the bedroom that evening before the deafening ringing in her ears began. There was a pit of silence in the room […] ",
-    content: " By Nadia Schluter ",
-    date: new Date( )
+    id: "4",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     },
     {
-    id: " 5 ",
-    title: " WhatsApp launches Channels feature for broadcast messages ",
-    imageUrl: " https://techcrunch.com/wp-content/uploads/2023/06/WhatsApp-channels.jpg?w=1390&crop=1 ",
-    description: " Meta is rolling out a broadcast-based messaging feature, called Channels, on WhatsApp, similar to a recent update it sent out to Instagram, as the social juggernaut experiments with giving more con... ",
-    content: " By Ivan Mehta ",
-    date: new Date( )
+    id: "5",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     },
     {
-    id: " 6 ",
-    title: " WOK FRIED PEANUTS / HOW TO FRY PEANUTS ",
-    imageUrl: "https://thewoksoflife.com/wp-content/uploads/2022/12/Chinese-fried-peanuts-appetizer-4.jpg",
-    description: "  You'll see these fried peanuts often in Cantonese restaurants, served in small dishes around a Lazy Susan along with pots of tea. They're the perfect little appetizer while you wait for your meal to arrive. Wok fried peanuts are an ideal... ",
-    content: " By JUDY ",
-    date: new Date( )
+    id: "6",
+    car_model: "",
+    product_image: "",
+    description: "",
+    price: "",
     }
 ];
 
 // LocalStorage
-localStorage.setItem('blogs', JSON.stringify(featuredPosts));
-
-// Adding to the a new blog
-addBtn.addEventListener('click', addBlog);
-
-// Add Button
-function addBlog(){
-    result.innerHTML = ""
-    event.preventDefault();
-    if(tempTitle.value, tempImg.value, tempDep.value, tempCon.value == ''){
-        alert('Please add in new data!')
-        adminList()
-    } else {
-        
-        featuredPosts.push({
-            id: tempID,
-               title: tempTitle.value,
-               imageUrl: tempImg.value,
-               description: tempDep.value,
-               content: tempCon.value,
-               date: new Date( )
-        });
-        tempID++;
-        tempTitle.value, tempImg.value, tempDep.value, tempCon.value = '';
-        localStorage.setItem('blogs', JSON.stringify(featuredPosts));
-        adminList();
-    }
-}
-
-// delete button
-function deleteButtons(){
-        delButton = [...document.querySelectorAll('.deletebtn')];
-        delButton.forEach((item)=>{
-            item.addEventListener('click', deleteBlog)
-
-        })
-    }
-    function deleteBlog(event){
-        let start = delButton.indexOf(event.target);
-        featuredPosts.splice(start, 1);
-        localStorage.setItem('blogs', JSON.stringify(featuredPosts))
-        
-    }
+localStorage.setItem('Car_Models', JSON.stringify(featured_products));
 
 
 
 // Table
-function adminList (){
-featuredPosts.forEach((data)=>{
-    result.innerHTML +=`
+function product_listing(){
+featured_products.forEach((data)=>{
+    display.innerHTML +=`
      <tr>
-        <td id="td1">${data.id}</td>
-        <td id="td2">${data.title}</td>
-        <td id="td3"><img class="image" src="${data.imageUrl}" loading="lazy" alt="${data.title}"></td>
-        <td id="td4">${data.description}</td>
-        <td id="td5">${data.content}</td>
-        <td id="td6">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Edit
-        </button>
-
+        <td id="product_id">${data.id}</td>
+        <td id="product_car_model">${data.car_model}</td>
+        <td id="product_image"><img class="image" src="${data.product_image}" loading="lazy" alt="${data.car_model}"></td>
+        <td id="product_description">${data.description}</td>
+        <td id="product_price">${data.price}</td>
+        <td id="product_edit_button">
+        <button type="button" class="btn btn-dark " data-bs-toggle="modal" data-bs-target="#exampleModal">EDIT</button>
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
             aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit this Blog</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Your Product:</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <p>Title</p>
-                        <input type="text"class="modal-input" placeholder="Enter Title..">
-                        <p>ImageURL</p>
-                        <input type="text" class="modal-input"  placeholder="Enter Image URL..">
-                        <p>Despcription</p>
-                        <input type="text" class="modal-input"  placeholder="Enter Description..">
-                        <p>Content</p>
-                        <input type="text" class="modal-input"  placeholder="Enter Content.">
+                        <p>Car model:</p>
+                        <input type="text"class="modal-input" id="input_1" placeholder="Tesla..">
+                        <p>Image:</p>
+                        <input type="" class="modal-input" id="input_2" placeholder="Insert image..">
+                        <p>Despcription:</p>
+                        <input type="text" class="modal-input" id="imput_3" placeholder="It's a car on 4 wheels..">
+                        <p>Price:</p>
+                        <input type="text" class="modal-input" id="input_4" placeholder="R20 000...">
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-dark">Save Changes</button>
+                        <button type="button" class="btn btn-dark" id="new_product">Add Edited Product</button>
                     </div>
                 </div>
             </div>
         </div>
         </td>
-        <td id="td7"><button type="button" class="btn btn-dark deletebtn" > Delete </button></td>
+        <td id="product_delete_"><button type="button" class="btn btn-dark" id="delete_button">DELETE</button></td>
      </tr>
     `
     
 })
-deleteButtons()
 }
+product_listing()
 
-adminList()
+
+
+// // Adding to the a new blog
+// addBtn.addEventListener('click', addBlog);
+
+// // Add Button
+// function addBlog(){
+//     result.innerHTML = ""
+//     event.preventDefault();
+//     if(product_model.value, product_image.value, product_description.value, product_price.value == ''){
+//         alert('Please add in new data!')
+//         adminList()
+//     } else {
+        
+//         featured_products.push({
+//             id: product_id,
+//                title: product_model.value,
+//                imageUrl: product_image.value,
+//                description: product_description.value,
+//                content: product_price.value,
+//                date: new Date( )
+//         });
+//         product_id++;
+//         product_model.value, product_image.value, product_description.value, product_price.value = '';
+//         localStorage.setItem('blogs', JSON.stringify(featured_products));
+//         adminList();
+//     }
+// }
+
+// // delete button
+// function deleteButtons(){
+//         delButton = [...document.querySelectorAll('.deletebtn')];
+//         delButton.forEach((item)=>{
+//             item.addEventListener('click', deleteBlog)
+
+//         })
+//     }
+//     function deleteBlog(event){
+//         let start = delButton.indexOf(event.target);
+//         featured_products.splice(start, 1);
+//         localStorage.setItem('blogs', JSON.stringify(featured_products))
+        
+//     }
+
