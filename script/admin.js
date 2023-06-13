db655e3a575bfcefbfb71f42a2fa5f72decc139d
KeenoFourie <keenofourie614@gmail.com>
2023-06-13 13:40:32 +0200

    edit button not done

--- script/admin.js
+++ script/admin.js
@@ -1,5 +1,7 @@
 let display = document.querySelector("#product_list");
 let addBtn = document.querySelector("#new_product");
+let del_button = document.querySelector("#delete_button")
+let edit_product_button = document.querySelector("#edit_products_button")
 let product_id = 7;
 let product_model = document.querySelector("#input_1");
 let product_image = document.querySelector("#input_2");
@@ -14,42 +16,42 @@ let  featured_products = [
     {
     id: "1",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     },
     {
     id: "2",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     },
     {
     id: "3",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     },
     {
     id: "4",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     },
     {
     id: "5",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     },
     {
     id: "6",
     car_model: "",
-    product_image: "",
+    image_url: "",
     description: "",
     price: "",
     }
@@ -59,117 +61,96 @@ let  featured_products = [
 localStorage.setItem("Car_Models", JSON.stringify(featured_products));
 
 
-
-function product_listing(){
-featured_products.forEach((data)=>{
-    display.innerHTML +=`
-     <tr>
-        <td id="product_id">${data.id}</td>
-        <td id="product_car_model">${data.car_model}</td>
-        <td id="product_image"><img class="image" src="${data.product_image}" loading="lazy" alt="${data.car_model}"></td>
-        <td id="product_description">${data.description}</td>
-        <td id="product_price">${data.price}</td>
-        <td id="product_edit_button">
-        <button type="button" class="btn btn-dark " data-bs-toggle="modal" data-bs-target="#exampleModal">EDIT</button>
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Edit Your Product:</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Car model:</p>
-                        <input type="text"class="modal-input" id="input_1" placeholder="Tesla..">
-                        <p>Image:</p>
-                        <input type="" class="modal-input" id="input_2" placeholder="Insert image..">
-                        <p>Despcription:</p>
-                        <input type="text" class="modal-input" id="imput_3" placeholder="It's a car on 4 wheels..">
-                        <p>Price:</p>
-                        <input type="text" class="modal-input" id="input_4" placeholder="R20 000...">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-dark" id="new_product">Add Edited Product</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </td>
-        <td id="product_delete_"><button type="button" class="btn btn-dark" id="delete_button">DELETE</button></td>
-     </tr>
-    `
-    
-})
-}
-product_listing()
-
 // Add button
 addBtn.addEventListener("click", add_products);
-function add_products(event) {
+function add_products() {
     display.innerHTML = ""
     event.preventDefault();
-    if(product_description.value, product_image.value, product_model.value, product_price.value = "" ) {
+    if(product_description.value == '', product_image.value == '', product_model.value == '', product_price.value == ''){
         alert("Please insert information on your car!")
         product_listing()
-    }
-    else{
+    } else {
         featured_products.push({
             id: product_id,
             car_model: product_model.value,
-            product_image: product_image.value,
+            image_url: product_image.value,
             description: product_description.value,
             price: product_price.value,
         });
         product_id++;
-        product_model.value, product_image.value, product_description.value, product_price.value = "";
+        product_description.value = "", product_image.value = "", product_model.value = "", product_price.value = "" ;
         localStorage.setItem("Car_Models", JSON.stringify(featured_products));
-        adminList();
+        product_listing();
     }
 }
 
 
-// // Adding to the a new blog
-// addBtn.addEventListener('click', addBlog);
 
-// // Add Button
-// function addBlog(){
-//     result.innerHTML = ""
-//     event.preventDefault();
-//     if(product_model.value, product_image.value, product_description.value, product_price.value == ''){
-//         alert('Please add in new data!')
-//         adminList()
-//     } else {
+// delete button
+function delete_button(){
+        del_button = [...document.querySelectorAll("#delete_button")];
+        del_button.forEach((products)=>{
+            products.addEventListener("click", delete_products)
+
+        })
+    }
+    function delete_products(event){
+        display.innerHTML = ""
+        let start_point = del_button.indexOf(event.target);
+        featured_products.splice(start_point, 1);
+        localStorage.setItem("Car_Models", JSON.stringify(featured_products));
+        product_listing()
+    }
+
+
+// edit button
+
+    
+    
+    function product_listing(){
+    featured_products.forEach((data)=>{
+        display.innerHTML +=`
+         <tr class="">
+            <td id="product_id">${data.id}</td>
+            <td id="product_car_model">${data.car_model}</td>
+            <td id="product_image"><img class="image" src="${data.image_url}" loading="lazy" alt="${data.car_model}"></td>
+            <td id="product_descriptions">${data.description}</td>
+            <td id="product_prices">${data.price}</td>
+            <td id="product_edit_button">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#staticBackdrop">EDIT</button>
+            
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Your Product:</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                          <p>Car model:</p>
+                          <input type="text"class="modal-input" placeholder="Tesla..">
+                          <p>Image:</p>
+                          <input type="text" class="modal-input" placeholder="Insert image..">
+                          <p>Despcription:</p>
+                          <input type="text" class="modal-input" placeholder="It's a car on 4 wheels..">
+                          <p>Price:</p>
+                          <input type="text" class="modal-input" placeholder="R20 000...">
+                      </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" id="edit_products_button">Edit</button>
+            </div>
+            </div>
+            </div>
+            </div>
+            </td>
+            <td id="product_delete_"><button type="button" class="btn btn-dark" id="delete_button">DELETE</button></td>
+         </tr>
+        `
         
-//         featured_products.push({
-//             id: product_id,
-//                title: product_model.value,
-//                imageUrl: product_image.value,
-//                description: product_description.value,
-//                content: product_price.value,
-//                date: new Date( )
-//         });
-//         product_id++;
-//         product_model.value, product_image.value, product_description.value, product_price.value = '';
-//         localStorage.setItem('blogs', JSON.stringify(featured_products));
-//         adminList();
-//     }
-// }
-
-// // delete button
-// function deleteButtons(){
-//         delButton = [...document.querySelectorAll('.deletebtn')];
-//         delButton.forEach((item)=>{
-//             item.addEventListener('click', deleteBlog)
-
-//         })
-//     }
-//     function deleteBlog(event){
-//         let start = delButton.indexOf(event.target);
-//         featured_products.splice(start, 1);
-//         localStorage.setItem('blogs', JSON.stringify(featured_products))
-        
-//     }
-
+    })
+    delete_button()
+    }
+    product_listing()
