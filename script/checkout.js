acb08cac71889f511c0855bb349714960f07fc4c
KeenoFourie <keenofourie614@gmail.com>
2023-06-13 16:29:54 +0200

    checkout incomplete

---
+++ script/checkout.js
@@ -0,0 +1,27 @@
+// // products page section
+// let checkout_page = [
+//     {
+//         id: "1",
+//         title: "ljfdn",
+//         imageUrl: "sjdfbk",
+//         description: "oufsdjcn",
+//         price: "oewfusdjl",
+//     },
+//     {
+//         id: "2",
+//         title: "ljfdn",
+//         imageUrl: "sjdfbk",
+//         description: "oufsdjcn",
+//         price: "oewfusdjl",
+//     }
+// ]
+// // display products page content
+// let checkout_items = document.querySelector("#checkout_content");
+// checkout_page.forEach((product)=> {
+//     checkout_items.innerHTML += `
+//     <div id="products_page_container">
+//             <img src="${product.imageUrl}" class="card-img-top" alt="images" loading="lazy">
+//             <h5 class="card-title">${product.price}</h5>
+//     </div>
+//     `
+// })
